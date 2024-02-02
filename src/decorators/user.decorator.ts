693290bc7ddee3cd1c3b6bd4043c840bffceafac
Priizy12 <ivailsonqueiroz9@gmail.com>
2024-02-02 12:30:19 -0300

    ajustando guard

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -4,7 +4,7 @@ export const User = createParamDecorator((_: unknown, context: ExecutionContext)
 
     const request = context.switchToHttp().getRequest()
 
-    if (request.user && request.empresa) {
+    if (request.user) {
         return request.user
     } else {
         throw new NotFoundException('Usuario nao encontrado no request. Use o AuthGuard para obter o usuario')
