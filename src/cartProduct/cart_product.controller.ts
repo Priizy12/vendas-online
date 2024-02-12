6ef742ecbe3afa5f86832471dcf3fbd0f641e1eb
Priizy12 <ivailsonqueiroz9@gmail.com>
2024-02-12 15:12:44 -0300

    atualizando permissoes e adicionando guards ao crud carrinhos de compra

--- src/cartProduct/cart_product.controller.ts
+++ src/cartProduct/cart_product.controller.ts
@@ -1,14 +1,15 @@
-import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put, UseGuards } from '@nestjs/common';
 import { CartProductService } from './cart_product.service';
 import { InserCartDto } from './dto/insert-cart.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { UpdateCartDto } from './dto/update-cart.dto';
 import { Paramid } from '../decorators/param-id.decorator';
+import { AuthGuard } from '../guards/auth.guard';
 
 
 
 
-
+@UseGuards(AuthGuard)
 @Controller('cart')
 @ApiTags('Controle de Carrinho de produtos.')
 export class CartProductController {
@@ -28,7 +29,7 @@ export class CartProductController {
    }
 
    @Put(':id')
-   async updateProduct (@Body() data: UpdateCartDto, @Paramid() id ) {
+   async updateProduct(@Body() data: UpdateCartDto, @Paramid() id) {
       return this.cartproductService.UpdateProduct(data, id)
    }
 
