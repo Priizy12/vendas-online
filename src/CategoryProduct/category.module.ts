430ef7a2828079b584dd84a11e78b684930c8b61
Priizy12 <ivailsonqueiroz9@gmail.com>
2024-02-20 10:32:23 -0300

    config categoria

--- src/CategoryProduct/category.module.ts
+++ src/CategoryProduct/category.module.ts
@@ -2,12 +2,14 @@ import { Module } from "@nestjs/common";
 import { CategoryProductService } from "./category.service";
 import { PrismaClient } from "@prisma/client";
 import { CategoryProductController } from "./category.controller";
+import { UsersModule } from "../users/users.module";
+import { AuthModule } from "../auth/auth.module";
 
 
 
 
 @Module({
-    imports: [],
+    imports: [UsersModule, AuthModule],
     providers: [CategoryProductService, PrismaClient],
     controllers: [CategoryProductController]
 })
