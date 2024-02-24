4350d1a14e4ffe597221b50611111017a0148f22
Priizy12 <ivailsonqueiroz9@gmail.com>
2024-02-24 15:37:04 -0300

    ajuste

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -4,21 +4,6 @@ import Stripe from 'stripe';
 @Injectable()
 export class PaymentService {
 
-  private stripe;
-
-  constructor(
-    private readonly cart: card_produtos,
-    private readonly product: Produtos,
-  ) {
-    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2023-10-16'
-      
-    })
-  }
-  async checkout(){
-   
-  }
-
 
 
 }
