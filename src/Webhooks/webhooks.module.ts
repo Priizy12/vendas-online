794c66794c807e43f7f1908245ecbc0d4fc460ce
Kayo Guilherme <Priizy12>
2024-06-20 13:15:30 -0300

    refactor: reajustando validação de webhook

--- src/Webhooks/webhooks.module.ts
+++ src/Webhooks/webhooks.module.ts
@@ -9,7 +9,6 @@ import { PrismaService } from "src/database/prisma.service";
 @Module({
     controllers: [WebhookController],
     providers: [PrismaService]
-    
 })
 
 export class WebHooksModule{}
